4cbfc9b5d81177f4a9cc36233cba2b5979d6806c
Swofty <admin@swofty.net>
2026-01-28 23:24:07 +1100

    refactor(MAC-244): reuse SanityImage type for CommitteeMember photo

--- lib/sanity/types.ts
+++ lib/sanity/types.ts
@@ -108,38 +108,12 @@ export type TeamSlug =
   | 'projects'
   | 'outreach'
 
-export interface CommitteeMemberImage {
-  asset: {
-    _id: string
-    url: string
-    metadata?: {
-      dimensions: {
-        width: number
-        height: number
-      }
-    }
-  }
-  alt?: string
-  hotspot?: {
-    x: number
-    y: number
-    height: number
-    width: number
-  }
-  crop?: {
-    top: number
-    bottom: number
-    left: number
-    right: number
-  }
-}
-
 export interface CommitteeMember {
   _id: string
   name: string
   role: string
   team: TeamSlug
-  photo?: CommitteeMemberImage
+  photo?: SanityImage
   pastRoles?: string[]
   bio?: string
   linkedIn?: string
