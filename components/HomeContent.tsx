a6be6fede0868f735b32480f8ced6f81957e0636
Jason Yi <jasonyi1413@gmail.com>
2026-02-22 04:36:42 +1100

    feat(MAC-283): add paginator loading reels

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -2,7 +2,6 @@
 
 import { HomepageSection, EventDocument, SocialLink } from '@/lib/sanity/types'
 import type { YouTubeVideo } from '@/lib/youtube/feed'
-import type { InstagramReel } from '@/lib/instagram/feed'
 import {
   StorySection,
   InstagramSection,
@@ -15,13 +14,12 @@ interface HomeContentProps {
   events?: EventDocument[]
   socialLinks?: SocialLink[]
   youtubeVideos?: YouTubeVideo[]
-  instagramReels?: InstagramReel[]
 }
 
-export function HomeContent({ sections, events, socialLinks, youtubeVideos, instagramReels }: HomeContentProps) {
+export function HomeContent({ sections, events, socialLinks, youtubeVideos }: HomeContentProps) {
   // If sections are provided from Sanity, render them in order
   if (sections && sections.length > 0) {
-    return <SectionRenderer sections={sections} events={events} socialLinks={socialLinks} youtubeVideos={youtubeVideos} instagramReels={instagramReels} />
+    return <SectionRenderer sections={sections} events={events} socialLinks={socialLinks} youtubeVideos={youtubeVideos} />
   }
 
   // Fallback to default sections if no Sanity data
