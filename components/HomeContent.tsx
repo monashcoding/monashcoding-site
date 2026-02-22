854ac04cb130aee796f7ee299e8999487f377a5c
Jason Yi <jasonyi1413@gmail.com>
2026-02-22 16:05:11 +1100

    feat(MAC-287): reuse sponsor section from sponsor us in home

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,29 +1,28 @@
 'use client'
 
-import { HomepageSection, EventDocument, SocialLink } from '@/lib/sanity/types'
+import { HomepageSection, EventDocument, SocialLink, SponsorPageData } from '@/lib/sanity/types'
 import type { YouTubeVideo } from '@/lib/youtube/feed'
-import {
-  SponsorsSection,
-  SectionRenderer,
-} from '@/components/home'
+import { SponsorLogosGrid } from '@/components/sponsor/SponsorLogosGrid'
+import { SectionRenderer } from '@/components/home'
 
 interface HomeContentProps {
   sections?: HomepageSection[]
   events?: EventDocument[]
   socialLinks?: SocialLink[]
   youtubeVideos?: YouTubeVideo[]
+  sponsorPageData?: SponsorPageData | null
 }
 
-export function HomeContent({ sections, events, socialLinks, youtubeVideos }: HomeContentProps) {
+export function HomeContent({ sections, events, socialLinks, youtubeVideos, sponsorPageData }: HomeContentProps) {
   // If sections are provided from Sanity, render them in order
   if (sections && sections.length > 0) {
-    return <SectionRenderer sections={sections} events={events} socialLinks={socialLinks} youtubeVideos={youtubeVideos} />
+    return <SectionRenderer sections={sections} events={events} socialLinks={socialLinks} youtubeVideos={youtubeVideos} sponsorPageData={sponsorPageData} />
   }
 
   // Fallback to default sections if no Sanity data
   return (
     <>
-      <SponsorsSection />
+      <SponsorLogosGrid title={sponsorPageData?.sponsorsTitle} sponsors={sponsorPageData?.sponsors} />
     </>
   )
 }
--- components/home/index.ts
+++ components/home/index.ts
@@ -1,4 +1,3 @@
-export { SponsorsSection } from './SponsorsSection'
 export { EventsSection } from './EventsSection'
 export { CommunitySection } from './CommunitySection'
 export { Footer } from './Footer'
