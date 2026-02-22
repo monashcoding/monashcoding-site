b9eb7fa80e47acb3f780981731bc1a825f688bd5
Jason Yi <jasonyi1413@gmail.com>
2026-02-22 18:41:49 +1100

    feat(MAC-287): disable dark reader extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
+  other: {
+    "darkreader-lock": "",
+  },
   title: "Monash Association of Coding",
   description: "At MAC, we aim to impart technical skills and industry-relevant experiences to students to bridge the gap between the classroom and industry. We want to make coding a fun experience for all—regardless of degree, year level, and experience—by providing collaborative learning opportunities for our members.",
   openGraph: {
