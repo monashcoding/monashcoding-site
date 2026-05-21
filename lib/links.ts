5e35310318cc5ca5d61d19931c1ed0d9146ee62f
Steven <spha0034@student.monash.edu>
2026-05-21 15:57:53 +1000

    feat: temporary buttons for projects

--- lib/links.ts
+++ lib/links.ts
@@ -1,2 +1,3 @@
 export const MEMBER_SIGNUP_URL = 'https://clubs.msa.monash.edu/organisation/7489/'
 export const JOB_BOARD_URL = 'https://jobs.monashcoding.com/'
+export const MONMAP_URL = 'https://monmap.monashcoding.com/'
